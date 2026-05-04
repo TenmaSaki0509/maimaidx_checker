e3c6677eb124fe98349946d809094739b48869c1
天马咲希的狗 <66952898+TenmaSaki0509@users.noreply.github.com>
2026-05-04 14:11:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ from astrbot.api.event import filter, AstrMessageEvent
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
 
-@register("astrbot_plugin_maicheck", "你的名字", "查询舞萌机房信息的插件", "1.0.0")
+@register("astrbot_plugin_maicheck", "冷沫", "查询舞萌机台信息", "1.0.0")
 class MaicheckPlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
 
     @filter.command("maicheck")
     async def maicheck(self, event: AstrMessageEvent):
-        '''查询舞萌机房的最新信息'''
+        '''查询国服舞萌机台的最新信息'''
         yield event.plain_result("正在查询，请稍后...")
         try:
             url = "https://wc.wahlap.net/maidx/location/"
@@ -24,13 +24,13 @@ class MaicheckPlugin(Star):
             # 示例: 查找带有特定class的div
             items = soup.select('div.location-item')  # 你需要确认这个选择器是否正确
             if items:
-                result_lines = ["【当前机房信息】"]
+                result_lines = ["【当前机台信息】"]
                 for item in items[:10]:  # 限制返回条数，避免消息过长
                     result_lines.append(f"- {item.get_text(strip=True)}")
                 result = "\n".join(result_lines)
             else:
                 # 如果找不到特定元素，可以返回页面标题等通用信息作为备选
-                page_title = soup.title.string if soup.title else "舞萌机房信息"
+                page_title = soup.title.string if soup.title else "舞萌机台信息"
                 result = f"当前获取到网页信息: {page_title}"
             # --------------------------------
             yield event.plain_result(result)
