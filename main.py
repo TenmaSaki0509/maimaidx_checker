c2d51be84d313b9912572b268f80eb9d24173be9
天马咲希的狗 <66952898+TenmaSaki0509@users.noreply.github.com>
2026-05-04 13:40:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,16 @@
+import aiohttp
+import asyncio
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
-from astrbot.api import logger
+from astrbot.api import logger  # 使用官方logger记录日志[reference:2]
+from bs4 import BeautifulSoup
+from astrbot.api.event import MessageChain  # 导入MessageChain用于构建主动消息[reference:3]
 
-@register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
-class MyPlugin(Star):
-    def __init__(self, context: Context):
+@register("maimaidx_checker", "冷沫", "查看国服舞萌机台的更新信息", "1.0.0")
+class MaimaiWatcher(Star):
+    def __init__(self, context: Context, config: dict = None):
         super().__init__(context)
+        self.last_content = ""
 
-    async def initialize(self):
-        """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
-
-    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
-    async def helloworld(self, event: AstrMessageEvent):
-        """这是一个 hello world 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
-        user_name = event.get_sender_name()
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
-
-    async def terminate(self):
-        """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
+        # 在后台启动一个异步任务，用于执行定时监控，这是实现每小时检查的关键[reference:4][reference:5]
+        asyncio.create_task(self.scheduled_fetch())
